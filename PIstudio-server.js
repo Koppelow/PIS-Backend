e1a3c0a7d233d03b8c6f3fdef8fbaceef510cb95
Koppelow <fredrikkoppeloweliassen@gmail.com>
2026-01-09 00:04:50 +0100

    bruh

--- PIstudio-server.js
+++ PIstudio-server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const https = require("https");
 const fs = require("fs");
-
+const cors = require("cors");
 const transformPrompt = require("./ai-backgrounds");
 
-const app = express();
 
+const app = express();
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 const apiKey = process.env.photoroomkey
