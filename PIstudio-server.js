5076e7d7db955318d4b39de07d5810daf2ff1b10
Koppelow <fredrikkoppeloweliassen@gmail.com>
2026-01-09 00:42:07 +0100

    adding cors again xd

--- PIstudio-server.js
+++ PIstudio-server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const https = require("https");
 const fs = require("fs");
+const cors = require("cors");
 const transformPrompt = require("./ai-backgrounds");
 
 
 const app = express();
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.options("/aibackground", cors());
+
 const apiKey = process.env.photoroomkey
 
 // Image editing response
